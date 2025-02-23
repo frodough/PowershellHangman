92a8d4ddd2f7cebe449cc444ce3c72d3700e9004
frodough <114837379+frodough@users.noreply.github.com>
2025-02-23 16:51:51 -0500

    Update hangman.ps1

--- hangman.ps1
+++ hangman.ps1
@@ -1,5 +1,65 @@
+cls
 $wins = 0
 $losses = 0
+
+$worddir = "$env:LOCALAPPDATA\Powershell Hangman"
+$wordfile = "words_categories.csv"
+
+write-host "Checking for word directory: " -nonewline
+sleep -seconds 1
+
+if (!(test-path $worddir)) {
+	write-host "Path $worddir not found" -fore yellow
+	sleep -seconds 1
+	
+	write-host "Creating directory: " -nonewline
+	sleep -seconds 1
+	
+	try {
+		new-item -itemtype directory -path $worddir -value "Powershell Hangman" | out-null
+		write-host "Directory created successfully" -fore green
+		sleep -seconds 1
+	}
+	catch {
+		write-host "Error directory creation failed" -fore red
+		sleep -seconds 1
+	}
+	
+	write-host "Fetching word list: " -nonewline
+	sleep -seconds 1
+	
+	try {
+		invoke-webrequest -uri "https://raw.githubusercontent.com/frodough/PowershellHangman/refs/heads/main/words_categories.csv" -outfile "$worddir\$wordfile"
+		write-host "Successfully downloaded word list" -fore green
+		sleep -seconds 1
+	}
+	catch {
+		write-host "Error fetching word list." -fore red
+		sleep -seconds 1
+	}
+} else {
+	write-host "Successfully found game directory" -fore green
+	sleep -seconds 1
+	write-host "Checking for word list: " -nonewline
+	sleep -seconds 1
+	
+	if (!(test-path "$worddir\$wordfile")) {
+		write-host "Wordlist not found" -fore yellow
+		
+		try {
+			invoke-webrequest -uri "https://raw.githubusercontent.com/frodough/PowershellHangman/refs/heads/main/words_categories.csv" -outfile "$worddir\$wordfile"
+			write-host "Successfully downloaded word list" -fore green
+			sleep -seconds 1
+		}
+		catch {
+			write-host "Error fetching word list." -fore red
+			sleep -seconds 1
+		}
+	} else {
+		write-host "Wordlist successfully found" -fore green
+		sleep -seconds 1
+	}
+}
 
 do {
 	$intro = @"
@@ -77,9 +137,9 @@ GAME OVER!
 "@
 
 	$levels = @{
-		"1" = @{ max = 6; minlength = 2; maxlength = 6; words = import-csv "C:\temp\words_categories.csv"; setting = "Easy" }
-		"2" = @{ max = 6; minlength = 2; maxlength = 25; words = import-csv "C:\temp\words_categories.csv"; setting = "Normal" }
-		"3" = @{ max = 4; minlength = 2; maxlength = 50; words = import-csv "C:\temp\words_categories.csv"; setting = "Hard" }
+		"1" = @{ max = 6; minlength = 2; maxlength = 6; words = import-csv "$worddir\$wordfile"; setting = "Easy" }
+		"2" = @{ max = 6; minlength = 2; maxlength = 25; words = import-csv "$worddir\$wordfile"; setting = "Normal" }
+		"3" = @{ max = 4; minlength = 2; maxlength = 50; words = import-csv "$worddir\$wordfile"; setting = "Hard" }
 	}
 
 	do {
@@ -169,7 +229,7 @@ GAME OVER!
 		write-host "`n$($man[6])"
 		write-host "Difficulity: $($levels[$difficulty]["setting"])"
 		write-host "`nWord: $($word)"
-    	write-host "YOU LOSER!!!!! The word was $($word) get a fucking dictionary or learn how to spell!!!"
+    	write-host "Nice try but the word was $($word), if your not too scared try again!"
 		$again = read-host "`nPlay again?"
 	} else {
         $wins++
@@ -181,7 +241,7 @@ GAME OVER!
 		write-host "`n$($man[$attempts])"
 		write-host "Difficulity: $($levels[$difficulty]["setting"])"
 		write-host "`nWord: $($word)"
-    	write-host "WOW what a little smarty pants we have here, want a fucking award or something?!"
+    	write-host "Congrats on guessing the word $($word)! You must be a superstar or the word was something a baby could guess!"
 		$again = read-host "`nPlay again?"
 	}
 } 
